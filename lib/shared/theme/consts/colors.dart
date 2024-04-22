d5f86bebc581e87bd7a7e433af4f6acef28d7e05
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-23 01:09:53 +0300

    place colors in abstract class

--- lib/shared/theme/consts/colors.dart
+++ lib/shared/theme/consts/colors.dart
@@ -1,15 +1,17 @@
 import 'dart:ui';
 
-const Color backgroundMain = Color(0xFF051b35);
-const Color backgroundPaper = Color(0xFF0C3057);
+abstract class AppColors {
+  static const Color backgroundMain = Color(0xFF051b35);
+  static const Color backgroundPaper = Color(0xFF0C3057);
 
-const Color primaryMain = Color(0xFFf79e2d);
-const Color primaryPaper = Color(0xFF6a362c);
+  static const Color primaryMain = Color(0xFFf79e2d);
+  static const Color primaryPaper = Color(0xFF6a362c);
 
-const Color textPrimary = Color(0xFFfffcfa);
-const Color textHelper = Color(0xFF928882);
+  static const Color textPrimary = Color(0xFFfffcfa);
+  static const Color textHelper = Color(0xFF928882);
 
-const Color statusWarning = Color(0xFFE53232);
-const Color statusCaution = Color(0xFFE56832);
-const Color statusInfo = Color(0xFF326FE5);
-const Color statusSuccess = Color(0xFF04B000);
+  static const Color statusWarning = Color(0xFFE53232);
+  static const Color statusCaution = Color(0xFFE56832);
+  static const Color statusInfo = Color(0xFF326FE5);
+  static const Color statusSuccess = Color(0xFF04B000);
+}
