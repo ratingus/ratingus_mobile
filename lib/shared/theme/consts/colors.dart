4b08748f1f015efa3d8acc23d023f73f27e1b245
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-22 23:54:38 +0300

    add colors consts

---
+++ lib/shared/theme/consts/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+const Color backgroundMain = Color(0xFF051b35);
+const Color backgroundPaper = Color(0xFF0C3057);
+
+const Color primaryMain = Color(0xFFf79e2d);
+const Color primaryPaper = Color(0xFF6a362c);
+
+const Color textPrimary = Color(0xFFfffcfa);
+const Color textHelper = Color(0xFF928882);
+
+const Color statusWarning = Color(0xFFE53232);
+const Color statusCaution = Color(0xFFE56832);
+const Color statusInfo = Color(0xFF326FE5);
+const Color statusSuccess = Color(0xFF04B000);
