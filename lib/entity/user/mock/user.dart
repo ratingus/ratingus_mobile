6c9e9e7fa206208b818b9b0731dbf97d9cca175f
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-26 22:00:25 +0300

    add base user classes

---
+++ lib/entity/user/mock/user.dart
@@ -0,0 +1,11 @@
+import 'package:ratingus_mobile/entity/user/model/user.dart';
+
+final currentUser = User(
+    id: 0,
+    login: 'Логин',
+    name: 'Имя',
+    surname: 'Фамилия',
+    patronymic: 'Отчество',
+    schoolId: 0,
+    classId: 0,
+    birthdate: DateTime.utc(2002, 2, 12));
