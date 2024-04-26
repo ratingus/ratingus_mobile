7ea4f2492622dbb36fec53df0f85a4aae4c6d46d
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-26 21:56:46 +0300

    add localisation and modify layout

--- lib/shared/router/layout.dart
+++ lib/shared/router/layout.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:ratingus_mobile/shared/router/router.dart';
+import 'package:ratingus_mobile/shared/theme/theme.dart';
 import 'package:ratingus_mobile/widget/bottom_navigation_bar/bottom_navigation_bar.dart';
 
 @RoutePage()
@@ -9,14 +11,22 @@ class LayoutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AutoTabsScaffold(
-      routes: const [
-        AnnouncementsRoute(),
-        ProfileRoute(),
-        // TODO: routes
-      ],
-      bottomNavigationBuilder: (_, tabsRouter) =>
-          RatingusBottomNavigationBar(onTap: tabsRouter.setActiveIndex),
-    );
+    return MaterialApp(
+        title: 'Ratingus',
+        theme: appThemeData,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: AutoTabsScaffold(
+          routes: const [
+            AnnouncementsRoute(),
+            ProfileRoute(),
+            // TODO: routes
+          ],
+          bottomNavigationBuilder: (_, tabsRouter) =>
+              RatingusBottomNavigationBar(onTap: tabsRouter.setActiveIndex),
+        ));
   }
 }
