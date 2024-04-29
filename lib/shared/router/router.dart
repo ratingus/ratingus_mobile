f0e0a6c849ccc04d7cb0f33d74fb46d560b1968c
burunyuu23 <ezzfvkoko@gmail.com>
2024-04-29 13:32:26 +0300

    move to main

--- lib/shared/router/router.dart
+++ lib/shared/router/router.dart
@@ -1,18 +1,18 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:ratingus_mobile/pages/announcements.dart';
+import 'package:ratingus_mobile/pages/main/announcements.dart';
 import 'package:ratingus_mobile/pages/auth/guard.dart';
 import 'package:ratingus_mobile/pages/auth/layout.dart';
 import 'package:ratingus_mobile/pages/auth/pages/login.dart';
 import 'package:ratingus_mobile/pages/auth/pages/registration.dart';
 import 'package:ratingus_mobile/pages/auth/routes.dart';
-import 'package:ratingus_mobile/pages/calendar.dart';
+import 'package:ratingus_mobile/pages/main/calendar.dart';
 import 'package:ratingus_mobile/pages/diary/layout.dart';
 import 'package:ratingus_mobile/pages/diary/pages/by_day.dart';
 import 'package:ratingus_mobile/pages/diary/pages/by_lesson.dart';
 import 'package:ratingus_mobile/pages/diary/pages/by_week.dart';
 import 'package:ratingus_mobile/pages/diary/routes.dart';
-import 'package:ratingus_mobile/pages/layout.dart';
-import 'package:ratingus_mobile/pages/profile.dart';
+import 'package:ratingus_mobile/pages/main/layout.dart';
+import 'package:ratingus_mobile/pages/main/profile.dart';
 import 'package:flutter/foundation.dart';
 
 part 'router.gr.dart';
